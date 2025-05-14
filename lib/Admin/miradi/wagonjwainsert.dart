2609669a87e03164c3a3bb74f17bd8ddf49fec6c
kitunga3322 <latifuibrahim64@gmail.com>
2025-05-14 21:40:01 +0300

    14/5/2025

---
+++ lib/Admin/miradi/wagonjwainsert.dart
@@ -0,0 +1,126 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+
+class Wagonjwainsert extends StatefulWidget {
+  const Wagonjwainsert({super.key});
+
+  @override
+  State<Wagonjwainsert> createState() => _WagonjwainsertState();
+}
+class _WagonjwainsertState extends State<Wagonjwainsert> {
+  TextEditingController name = TextEditingController();
+  TextEditingController description = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  File? uploadimage;
+
+  Future<void> choosImage() async {
+    final XFile? chooseimage = await _picker.pickImage(source: ImageSource.gallery);
+    if (chooseimage != null) {
+      setState(() {
+        uploadimage = File(chooseimage.path);
+      });
+    }
+  }
+
+  Future<void> uploadData() async {
+    var uploadurl = Uri.parse('http://192.168.69.4/CHANGIA/wajaneinsert.php');
+    try {
+      if (uploadimage == null) {
+        print("Please select an image");
+        return;
+      }
+      List<int> imageBytes = uploadimage!.readAsBytesSync();
+      String baseimage = base64Encode(imageBytes);
+      var response = await http.post(uploadurl, body: {
+        "image": baseimage,
+        "name": name.text,
+        "description": description.text,
+      });
+      if (response.statusCode == 200) {
+        final jsondata = jsonDecode(response.body);
+        if (jsondata["error"]) {
+          print(jsondata["msg"]);
+        } else {
+          print("upload successfully");
+          // Optionally clear the fields after successful upload
+          setState(() {
+            name.clear();
+            description.clear();
+            uploadimage = null;
+          });
+        }
+      } else {
+        print("Error during connection to the server: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error during connection to the server: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Add wajane")),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: name,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Jina la mjane",
+                  hintText: "Andika Jina la mjane",
+                  prefixIcon: Icon(Icons.title),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: description,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "tatizo la mjane",
+                  hintText: "Andika tatizo la mjane",
+                  prefixIcon: Icon(Icons.text_snippet_outlined),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: uploadimage == null
+                  ? const Text("No image selected")
+                  : SizedBox(
+                height: 150,
+                child: Image.file(uploadimage!),
+              ),
+            ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  choosImage();
+                },
+                icon: const Icon(Icons.folder_open),
+                label: const Text("CHOOSE IMAGE")),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+                onPressed: () {
+                  uploadData();
+                },
+                icon: const Icon(Icons.upload),
+                label: const Text("UPLOAD DATA"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/User/miradi/yatima.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:latifu/Admin/miradi/yatimainsert.dart';
+
+class yatimalist extends StatefulWidget {
+  yatimalist({super.key});
+  @override
+  State<yatimalist> createState() => _yatimalistState();
+}
+
+class _yatimalistState extends State<yatimalist> {
+  late List<dynamic> yatimadata = [];
+  Future<void> getyatimaData() async {
+    try {
+      final response = await http.post(Uri.parse('http://192.168.69.4/CHANGIA/yatimalist.php'));
+      if (response.statusCode == 200) {
+        setState(() {
+          yatimadata = jsonDecode(response.body);
+        });} else {
+        print("Failed to load yatima data: ${response.statusCode}");
+      }} catch (e) {
+      print("Error fetching yatima data: $e");
+    }}
+  @override
+  void initState() {
+    super.initState();
+    getyatimaData();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text("YATIMA"), centerTitle: true,
+      actions: <Widget>[
+        IconButton(onPressed: () {Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext context) => const yatimainsert()
+            ,),
+        );
+        },
+          icon: const Icon(Icons.add_circle_outline_sharp, color: Colors.white,),),],),
+      body: FutureBuilder<void>(
+        future: null, builder: (context, AsyncSnapshot<void> snapshot) {
+        if (yatimadata.isNotEmpty) {
+          return ListView.builder(itemCount: yatimadata.length,
+            itemBuilder: (BuildContext context, int index) {
+              final String imageUrl = 'http://192.168.69.4/CHANGIA/${yatimadata[index]["image"]}'; // Corrected URL
+              return Card(
+                shape: const BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                color: Colors.indigo, clipBehavior: Clip.antiAliasWithSaveLayer,
+                margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.network(
+                      imageUrl, fit: BoxFit.fill, width: double.infinity, height: 200,
+                      errorBuilder: (context, error, stackTrace) {
+                        return SizedBox(height: 200, width: double.infinity, child: const Center(
+                          child: Text('Failed to load image'),
+                        ),);},),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text('${yatimadata[index]["name"]}: ${yatimadata[index]["description"]}',
+                        style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16,),),),],),);},);
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return const Center(child: Text('No data available.'));
+        }},),);}}
